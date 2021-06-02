0372d11c3b2dc064f9d1f262bcd459fcb5c7933c
AgustinaBehotas <agustinabehotas@gmail.com>
2021-06-02 15:38:54 -0300

    Update AppCompras.js
    Creating the routes

--- AppCompras.js
+++ AppCompras.js
@@ -1,5 +1,11 @@
 var express = requiere('express');
-var route = express.Router();
+var router = express.Router();
 var ctrlCompras = requiere('../controllers/compras.js');
 
-route.get('../controllers.js', )
+router.post('/compras', ctrlCompras.comprasCreate);
+router.get('/compras/:nroCompra', ctrlCompras.comprasReadOne);
+router.put('/compras/:nroCompra', ctrlCompras.comprasUpdateOne);
+router.delete('/compras/:nroCompra', ctrlCompras.comprasDeleteOne);
+router.get('/compras', ctrlCompras.comprasListByFormaDePago);
+
+export default router;
